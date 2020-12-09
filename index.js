ec081c1dfe0d68923697884992ce7d039af0f971
Igor M <mail@garik.site>
2020-12-10 03:24:48 +0400

    deploy

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Buytex = require('./lib/buytex');
 const Api = new Buytex()
 
+let price = 0.20
 
 Api.sendReq('exchangeGlobal/PlatformSpotOrderBookGroupInfo', 'symbol=BUX_USDT&limit=1000')
 .then(
   orderBook => {
     let sell = orderBook.sell
     let sum_usdt = 0
-    let price = 0.1
     for (let i = 0; i <= sell.length; i++) {
       if (i == sell.length) {
         sum_usdt = parseFloat(sum_usdt) * 0.25
