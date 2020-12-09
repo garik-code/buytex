b0e75d36e94d43ad12f68a359c32a7227d22622d
Igor M <mail@garik.site>
2020-12-10 03:29:40 +0400

    sum

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const Buytex = require('./lib/buytex');
 const Api = new Buytex()
 
-let price = 0.20
+const go = (price) => {
+  return new Promise((resolve, reject) => {
+    Api.sendReq('exchangeGlobal/PlatformSpotOrderBookGroupInfo', 'symbol=BUX_USDT&limit=1000')
+    .then(
+      orderBook => {
+        let sell = orderBook.sell
+        let sum_usdt = 0
+        for (let i = 0; i <= sell.length; i++) {
+          if (i == sell.length) {
+            sum_usdt = parseFloat(sum_usdt) * 0.25
+            resolve(`${price} USDT = ${sum_usdt} USDT`);
+          }else if (parseFloat(sell[i][0]) < price) {
+            sum_usdt = parseFloat(sum_usdt) + ( parseFloat(sell[i][0]) * parseFloat(sell[i][1]) )
+          }
+        }
+      },
+      err => reject(err)
+    )
+  });
+}
 
-Api.sendReq('exchangeGlobal/PlatformSpotOrderBookGroupInfo', 'symbol=BUX_USDT&limit=1000')
-.then(
-  orderBook => {
-    let sell = orderBook.sell
-    let sum_usdt = 0
-    for (let i = 0; i <= sell.length; i++) {
-      if (i == sell.length) {
-        sum_usdt = parseFloat(sum_usdt) * 0.25
-        console.log(`До цены ${price} = ${sum_usdt} USDT`);
-      }else if (parseFloat(sell[i][0]) < price) {
-        sum_usdt = parseFloat(sum_usdt) + ( parseFloat(sell[i][0]) * parseFloat(sell[i][1]) )
-      }
-    }
-  }
-)
+go(0.269500).then(go => console.log(go))
+go(0.117787).then(go => console.log(go))
+go(0.082053).then(go => console.log(go))
+go(0.070171).then(go => console.log(go))
+go(0.068686).then(go => console.log(go))
