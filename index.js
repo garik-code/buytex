d8105055bd838ffaa1c2ea129e0d4113ad9405be
Igor M <mail@garik.site>
2020-12-10 03:22:07 +0400

    deploy

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const Buytex = require('./lib/buytex');
 const Api = new Buytex()
 
-Api.sendReq('exchangeGlobal/PlatformSpotOrderBookList', 'symbol=BUX_USDT&orderID=0&limit=1000&sort=DESC')
+
+Api.sendReq('exchangeGlobal/PlatformSpotOrderBookGroupInfo', 'symbol=BUX_USDT&limit=1000')
 .then(
-  walletList => {
-    // console.log(walletList);
-    for (let i = 0; i < walletList.length; i++) {
-      if (walletList[i].side == 'SELL') { // BUY
-        console.log(`price: ${walletList[i].price}`);
-        console.log(`quantity: ${walletList[i].quantity}`);
-        console.log(`side: ${walletList[i].side}`);
-        console.log('----');
+  orderBook => {
+    let sell = orderBook.sell
+    let sum_usdt = 0
+    let price = 0.1
+    for (let i = 0; i <= sell.length; i++) {
+      if (i == sell.length) {
+        sum_usdt = parseFloat(sum_usdt) * 0.25
+        console.log(`До цены ${price} = ${sum_usdt} USDT`);
+      }else if (parseFloat(sell[i][0]) < price) {
+        sum_usdt = parseFloat(sum_usdt) + ( parseFloat(sell[i][0]) * parseFloat(sell[i][1]) )
       }
     }
   }
