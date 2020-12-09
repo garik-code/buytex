862d8e4594638f3dc2834b6f76f38e42437ea9e7
Igor M <mail@garik.site>
2020-12-10 03:07:02 +0400

    PlatformSpotOrderBookList https://buytex.io/Exchange/BUX_USDT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Buytex = require('./lib/buytex');
 const Api = new Buytex()
 
-Api.sendReq('wallet/WalletList', '')
+Api.sendReq('exchangeGlobal/PlatformSpotOrderBookList', 'symbol=BUX_USDT&orderID=0&limit=1000&sort=DESC&')
 .then(
   walletList => {
     console.log(walletList);
