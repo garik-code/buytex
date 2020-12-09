0d793719d8a3dfcfb7dc82f1ab986457eb0b0ed9
Igor M <mail@garik.site>
2020-12-10 03:11:47 +0400

    fix arguments

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Buytex = require('./lib/buytex');
 const Api = new Buytex()
 
-Api.sendReq('exchangeGlobal/PlatformSpotOrderBookList', 'symbol=BUX_USDT&orderID=0&limit=1000&sort=DESC&')
+Api.sendReq('exchangeGlobal/PlatformSpotOrderBookList', 'symbol=BUX_USDT&orderID=0&limit=1000&sort=DESC')
 .then(
   walletList => {
-    console.log(walletList);
+    // console.log(walletList);
+    for (let i = 0; i < walletList.length; i++) {
+      if (walletList[i].side == 'SELL') { // BUY
+        console.log(`price: ${walletList[i].price}`);
+        console.log(`quantity: ${walletList[i].quantity}`);
+        console.log(`side: ${walletList[i].side}`);
+        console.log('----');
+      }
+    }
   }
 )
