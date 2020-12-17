b85f3dcb8eb3db4ab01f81e7c56e0d3f81747e2d
Igor M <mail@garik.site>
2020-12-17 21:44:14 +0300

    deploy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const Calc = require('./lib/calc');
+const Calc = require('./lib/calc')
 const Calculator = new Calc()
 
-Calculator.dump(0.01).then(go => console.log(go))
-Calculator.pump(0.283).then(go => console.log(go))
+Calculator.dump(0.0156).then(go => console.log(go))
